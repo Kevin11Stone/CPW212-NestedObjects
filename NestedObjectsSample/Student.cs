58628ce5d5f8d11e803386d2f5b6c2c0f0261d50
ruca6616 <ruca6616@gmail.com>
2019-07-15 14:15:25 -0700

    Added documentation to the student class

--- NestedObjectsSample/Student.cs
+++ NestedObjectsSample/Student.cs
@@ -6,12 +6,25 @@ using System.Threading.Tasks;
 
 namespace NestedObjectsSample
 {
+    /// <summary>
+    /// Represents an individual student
+    /// </summary>
     class Student
     {
+        /// <summary>
+        /// The legal first and last names
+        /// ex. Jane Doe
+        /// </summary>
         public string FullName { get; set; }
 
+        /// <summary>
+        /// The student's school provided email address
+        /// </summary>
         public string EmailAddress { get; set; }
 
+        /// <summary>
+        /// Student's mailing address
+        /// </summary>
         public string HomeAddress { get; set; }
 
         /// <summary>
