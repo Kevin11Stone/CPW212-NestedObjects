6068cf3eebb3f04d9708d97c623111d6ae65f507
ruca6616 <ruca6616@gmail.com>
2019-07-17 14:35:08 -0700

    Implemented test data and basic functional prototype

--- NestedObjectsSample/Form1.cs
+++ NestedObjectsSample/Form1.cs
@@ -16,5 +16,96 @@ namespace NestedObjectsSample
         {
             InitializeComponent();
         }
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Instructor> instructors = new List<Instructor>();
+
+            PopulateInstructorTestData(instructors);
+
+            // data-binding using dataSource, DisplayMember is what will be shown
+            // in the listBox
+            lstInstructors.DataSource = instructors;
+
+            // nameof() operator to rid of strings as well as refactoring support/intellisense
+            lstInstructors.DisplayMember = nameof(Instructor.FullName);
+
+        }
+
+
+
+
+        private void PopulateInstructorTestData(List<Instructor> instructors)
+        {
+            Instructor instructorKen = new Instructor()
+            {
+                Email = "Ken@cptc.edu",
+                FullName = "Dr. Kenneth Meerdink"
+            };
+
+            List<Course> kenCourses = new List<Course>()
+            {
+                new Course()
+                {
+                    Title = "Data Structures",
+                    CourseNumber = "CPW 245",
+                    Roster = new List<Student>()
+                    {
+                        new Student("Jim Halpert"),
+                        new Student("Pam Halpert")
+                    }
+                },
+                new Course()
+                {
+                    Title = "Java II",
+                    CourseNumber = "CPW 143",
+                    Roster = new List<Student>()
+                    {
+                        new Student("Dwight Schrute")
+                    }
+                }
+            };
+
+            instructorKen.CourseLoad = kenCourses;
+            
+            instructors.Add(instructorKen);
+
+        }
+
+
+
+
+        private void lstInstructors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Instructor currInstructor = lstInstructors.SelectedItem as Instructor;
+
+            List<Course> courses = currInstructor.CourseLoad;
+
+            lstCourses.DataSource = courses;
+
+            // displayMember must be a single property
+            lstCourses.DisplayMember = nameof(Course.CourseDisplay);
+
+        }
+
+        private void lstCourses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // if no course is selected, return
+            if (lstCourses.SelectedIndex < 0)
+            {
+                return;
+            }
+
+            Course selectedCourse = lstCourses.SelectedItem as Course;
+
+            lstStudents.DataSource = selectedCourse.Roster;
+
+            lstStudents.DisplayMember = nameof(Student.FullName);
+
+        }
     }
 }
